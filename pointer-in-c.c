7bdbb0e031f48cf203f0021c1aac4fb93dc7a71c
Mathews Peter <mathewspeter27@gmail.com>
2021-04-13 12:11:42 +0530

    pointer

---
+++ pointer-in-c.c
@@ -0,0 +1,30 @@
+/*
+ * pointer-in-c.c
+ *
+ *  Created on: 13 Apr 2021
+ *      Author: mathewspeter
+ */
+
+
+#include <stdio.h>
+
+void update(int *a,int *b)
+{
+    int s = *a + *b;
+    int d = *a - *b;
+    if(d<0)
+        d*=-1;
+    *a = s;
+    *b = d;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
