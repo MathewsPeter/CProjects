c03b8dbdaf7b509a6364b2fa115972060e245f6d
Mathews Peter <mathewspeter27@gmail.com>
2021-04-17 01:01:12 +0530

    sutdent-mark-sum

---
+++ student-mark-sum.c
@@ -0,0 +1,28 @@
+/*
+ * student-mark-sum.c
+ *
+ *  Created on: 17 Apr 2021
+ *      Author: mathewspeter
+ */
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+//Complete the following function.
+
+int marks_summation(int* marks, int number_of_students, char gender) {
+  //Write your code here.
+  int offs=0;
+  if(gender=='g')
+        offs++;
+   int sum=0;
+   for(int i=0+offs; i<number_of_students; i+=2)
+        sum+=marks[i];
+   return sum;
+
+}
+
+int main() {
