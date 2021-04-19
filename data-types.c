bd9cc739b7636ea3c5c74af00fe05f4fecb55994
Mathews Peter <mathewspeter27@gmail.com>
2021-04-20 01:16:21 +0530

    data type

---
+++ data-types.c
@@ -0,0 +1,15 @@
+/*
+ * data-types.c
+ *
+ *  Created on: 20 Apr 2021
+ *      Author: mathewspeter
+ */
+
+
+
+int ii; double dd; char ss[100];
+scanf("%d\n%lf\n\n",&ii,&dd);
+gets(ss);
+printf("%d\n",(i+ii));
+printf("%.1lf\n",(d+dd));
+printf("%s",s);printf("%s",ss);
