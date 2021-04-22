615c134767083a4e9832b14515fa4acad679b8b4
Mathews Peter <mathewspeter27@gmail.com>
2021-04-22 21:32:13 +0530

    class-vs-instance

---
+++ class-vs-instance.c
@@ -0,0 +1,23 @@
+/*
+ * class-vs-instance.c
+ *
+ *  Created on: 22 Apr 2021
+ *      Author: mathewspeter
+ */
+
+
+  if (age<13) cout<<"You are young.\n";
+else if (age<18) cout<<"You are a teenager.\n";
+else cout<<"You are old.\n";
+
+
+
+
+  if(initialAge<0) {cout<<"Age is not valid, setting age to 0.\n";
+     age=0;}
+     else age = initialAge;
+
+
+
+
+  age+=1;
